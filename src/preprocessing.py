c17364780f458d0317486a438de003ff6006cf65
pierluigicostanzocg <128045754+pierluigicostanzocg@users.noreply.github.com>
2023-07-19 13:43:55 +0200

    Update preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -5,14 +5,14 @@ This module contains functions that help with preprocessing of the Huggingface a
 """
 
 import numpy as np
-from transformers import ASTFeatureExtractor
+from transformers import AutoFeatureExtractor
 from typing import Dict, Any, List
 
 
 def calculate_stats(examples: Dict[str, Dict[str, Any]], 
                     audio_field: str, 
                     array_field: str,
-                    feature_extractor: ASTFeatureExtractor) -> Dict[str, List[float]]:
+                    feature_extractor: AutoFeatureExtractor) -> Dict[str, List[float]]:
     """
     Calculates the mean and standard deviation of the spectrogram of the audio examples in the provided batch.
 
@@ -21,7 +21,7 @@ def calculate_stats(examples: Dict[str, Dict[str, Any]],
             field containing an audio array, and a label field containing a label value.
         audio_field (str): The name of the field in the examples that contains the audio file information.
         array_field (str): The name of the field in the audio_fielf that contains the audio arrays.
-        feature_extractor (ASTFeatureExtractor): An instance of the Hugging Face feature extractor to be used.
+        feature_extractor (AutoFeatureExtractor): An instance of the Hugging Face feature extractor to be used.
 
     Returns:
         Dict[str, List[float]]: A dictionary containing two keys: 'mean' and 'std', each with a 
@@ -39,7 +39,7 @@ def calculate_stats(examples: Dict[str, Dict[str, Any]],
 def preprocess_audio_arrays(examples: Dict[str, Any], 
                             audio_field: str,
                             array_field: str,
-                            feature_extractor: ASTFeatureExtractor) -> Dict[str, Any]:
+                            feature_extractor: AutoFeatureExtractor) -> Dict[str, Any]:
     """
     This function takes in a dictionary of audio examples and preprocesses them using a Hugging Face feature extractor.
     
@@ -48,7 +48,7 @@ def preprocess_audio_arrays(examples: Dict[str, Any],
             field containing an audio array, and a label field containing a label value.
         audio_field (str): The name of the audio field in the example dictionaries.
         array_field (str): The name of the audio array field within the audio field.
-        feature_extractor (ASTFeatureExtractor): The Hugging Face feature extractor to use for preprocessing the audio.
+        feature_extractor (AutoFeatureExtractor): The Hugging Face feature extractor to use for preprocessing the audio.
     
     Returns:
         Dict[str, Any]: Dictionary containing the preprocessed audio inputs as tensors. The key of the dictionary is
